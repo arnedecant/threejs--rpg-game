b3d6703321bf3305d185b072bf13a40786b60fa4
Arne Decant <hello@arnedecant.be>
2020-03-08 14:19:05 +0100

    add demo environment and intersect

--- src/js/game.js
+++ src/js/game.js
@@ -49,6 +49,8 @@ export default class Game {
             assets: this.assets
         })
 
+        this.createDummyEnvironment()
+
         this.player.onLoadingFinished.addListener(this.onPlayerLoaded.bind(this))
 
         // this.render()
@@ -63,6 +65,88 @@ export default class Game {
 
     }
 
+    createDummyEnvironment() {
+
+		const env = new THREE.Group()
+		env.name = "Environment"
+		ENGINE.scene.add(env)
+		
+		const geometry = new THREE.BoxBufferGeometry(150, 150, 150)
+		const material = new THREE.MeshBasicMaterial({ color: 0x333333 })
+		
+		for (let x = -1000; x < 1000; x += 300) {
+			for (let z =- 1000; z < 1000; z += 300) {
+
+				const block = new THREE.Mesh(geometry, material)
+                
+                block.position.set(x, 75, z)
+				env.add(block)
+            
+            }
+		}
+		
+        this.environmentProxy = env
+        
+    }
+
+    checkIntersection(options = {}) {
+
+        let playerPos = options.playerPos || PLAYER.model.position.clone()
+        let direction = options.direction || PLAYER.model.getWorldDirection()
+        let threshold = options.threshold || 50
+
+        let modifier = options.playerPosMod || { x: 0, y: 0, z: 0 }
+        
+        for (let axis in modifier) playerPos[axis] += modifier[axis]
+
+        const raycaster = new THREE.Raycaster(playerPos, direction)
+        
+        for (let box of this.environmentProxy.children) {
+
+            const intersect = raycaster.intersectObject(box)
+
+            if (intersect.length <= 0) continue
+            if (intersect[0].distance < threshold) return intersect[0]
+
+        }
+
+        return false
+
+    }
+    
+    movePlayer(dt) {
+        
+        let intersect = false
+        let playerPos = PLAYER.model.position.clone()
+        let direction = PLAYER.model.getWorldDirection()
+
+        playerPos.y += 60
+
+        // front
+        
+        intersect = this.checkIntersection({ playerPos, direction })
+        if (!intersect && this.player.move.z > 0) this.player.model.translateZ(dt * 100)
+        
+        // left
+
+        // direction.set(-1, 0, 0)
+        // direction.applyMatrix4(this.player.model.matrix)
+        // direction.normalize()
+
+        // intersect = this.checkIntersection({ playerPos, direction, threshold: 80 })
+        // if (!intersect) this.player.model.translateX(0 - (intersect.distance - 80))
+
+        // // right
+
+        // direction.set(1, 0, 0)
+		// direction.applyMatrix4(this.player.model.matrix)
+        // direction.normalize()
+        
+        // intersect = this.checkIntersection({ playerPos, direction, threshold: 80 })
+        // if (!intersect) this.player.model.translateX(intersect.distance - 80)
+
+    }
+
 	render() {
 
         const dt = CLOCK.getDelta()
@@ -70,7 +154,8 @@ export default class Game {
         if (this.player.mixer && this.mode == MODES.ACTIVE) this.player.mixer.update(dt)
 		
 		if (this.player.move && this.player.model) {
-			if (this.player.move.z > 0) this.player.model.translateZ(dt * 100)
+            this.movePlayer(dt)
+			// if (this.player.move.z > 0) this.player.model.translateZ(dt * 100)
 			this.player.model.rotateY(this.player.move.x * -dt)
         }
         
